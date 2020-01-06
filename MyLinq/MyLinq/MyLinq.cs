b2387b11cf9772da8800ad1ae0df1ffa20edc9ca
Minh Sang Tran <sangtran.d14ptit@outlook.com>
2020-01-06 11:57:19 +0700

    Thêm project demo tự viết method Where của Linq

---
+++ MyLinq/MyLinq/MyLinq.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLinq
+{
+    public static class MyLinq
+    {
+        // Extension method
+        public static IEnumerable<TSource> MyWhere<TSource> (this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            // Loop toàn bộ item trong danh sách truyền vào
+            foreach (var item in source)
+            {
+                // Callback lại hàm đã truyền vào, hàm này trả ra giá trị boolean
+                // Nếu hàm callback return true, đưa item vào IENumerable kết quả
+                if (predicate(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
---
+++ MyLinq/MyLinq/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> intList = new List<int> { 1, 2, 3};
+            var result = intList.MyWhere(i => i < 3);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
