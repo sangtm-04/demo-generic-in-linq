59c24b735a5e8d3aade08c5b8b501fb46f96b7fc
Minh Sang Tran <sangtran.d14ptit@outlook.com>
2020-01-06 11:22:07 +0700

    Sửa tên thư mục. Lưu tất cả demo liên quan đến LINQ vào chung folder

---
+++ AnonymousFunctionAndLambdaExpressionInLINQ/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Student
+{
+	public string Name { get; set; }
+	public int Age { get; set; }
+}
+
+public class Program
+{
+	public static void Main()
+	{
+		var studentList = new List<Student>();
+
+		var students = studentList.Where(stu => stu.Age > 20);
+		//							    \-Lambda Expression--/
+		//                          \----------LINQ----------/
+
+		var student = studentList.Where(delegate (Student stu) { return stu.Age > 20; });
+		//								\-------------Anonymous function----------------/
+		//                         \------------------------LINQ------------------------/
+	}
+}
---
+++ ExtensionMethodInLINQ/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class ListExt
+{
+	public static T randomElement<T>(this List<T> list)
+	{
+		Random ran = new Random();
+		int randomIndex = ran.Next(list.Count - 1);
+		return list[randomIndex];
+	}
+}
+
+public class Program
+{
+	public static void Main()
+	{
+		List<int> intList = new List<int> { 1, 2, 3, 4, 5, 6 };
+		List<double> doubleList = new List<double> { 0.1, 0.2, 0.3, 0.4, 0.5, 0.6 };
+
+		// Sử dụng method generic
+		var randomInt = getRandomElement(intList);
+		var randomDouble = getRandomElement(doubleList);
+		Console.WriteLine(randomInt);
+		Console.WriteLine(randomDouble);
+
+		// Extension method
+		Console.WriteLine(intList.randomElement());
+		Console.WriteLine(doubleList.randomElement());
+	}
+
+	public static T getRandomElement<T>(List<T> list)
+	{
+		Random ran = new Random();
+		int randomIndex = ran.Next(list.Count - 1);
+		return list[randomIndex];
+	}
+}
